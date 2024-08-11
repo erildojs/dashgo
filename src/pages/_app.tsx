1b4d50c8378ce1f83ff0b0d7dc0f33533a129ad0
ErildoJS <erildofranciscojs@gmail.com>
2024-08-11 12:34:38 +0100

    add model user with miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,13 @@ import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 
 const roboto = Roboto({ subsets: ['latin'], weight: '500' })
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer()
+}
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
