3bf3cd792ce0018145e167979b5425f4c3192500
ErildoJS <erildofranciscojs@gmail.com>
2024-08-22 19:11:09 +0100

    add create user with mutations of tanstack

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -6,6 +6,10 @@ import Link from "next/link";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { SubmitHandler, useForm } from 'react-hook-form';
+import { useMutation } from "react-query";
+import { api } from "../../services/api";
+import { queryClient } from "../../services/queryClient";
+import { useRouter } from "next/router";
 
 type CreateUserFormData = {
   name: string
@@ -15,7 +19,7 @@ type CreateUserFormData = {
 }
 
 const createUserFormSchema = yup.object().shape({
-  name: yup.string().email('Nome obrigatorio'),
+  name: yup.string().required('Nome obrigatorio'),
   email: yup.string().email('E-mail obrigatorio').required('E-mail invalido'),
   password: yup.string().required('Senha obrigatoria').min(6, 'No minimo 6 caracteres'),
   password_confirmation: yup.string().oneOf([
@@ -24,16 +28,28 @@ const createUserFormSchema = yup.object().shape({
 })
 
 export default function CreateUser() {
+  const router = useRouter()
+  const createUser = useMutation(async (user: CreateUserFormData) => {
+    const response = await api.post('users', {
+      user: {
+        ...user,
+        created_at: new Date()
+      }
+    })
+    return response.data.user
+  }, {
+    onSuccess: () => {
+      queryClient.invalidateQueries('users')
+    }
+  })
   const { register, handleSubmit, formState } = useForm({
     resolver: yupResolver(createUserFormSchema)
   })
   const { errors } = formState
-  const handleSignIn: SubmitHandler<CreateUserFormData> = (values) => {
 
-  }
-
-  const handleCreateUser: SubmitHandler<CreateUserFormData> = (values) => {
-
+  const handleCreateUser: SubmitHandler<CreateUserFormData> = async (values) => {
+    await createUser.mutateAsync(values)
+    router.push('/users')
   }
 
   return (
